c81a39e97347bc8ac0cb928460fbf0d9c03a696f
ginabradys <ginabradys@users.noreply.github.com>
2020-04-02 23:14:11 +0200

    added new files

---
+++ SDGmainpage.js
@@ -0,0 +1,19 @@
+ "language": {
+        "javascript": {
+            "linting.prefer": [
+                "JSHint"
+            ],
+            "linting.usePreferredOnly": true
+        }
+    }
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
